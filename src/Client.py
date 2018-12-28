f5ba74dcf3cb780d5f2192d697390b73ebacfe5d
Daniel Burns <danieljburns143@gmail.com>
2018-12-28 03:35:56 +0000

    Fix: Client class no longer implements channels and product IDs

--- src/Client.py
+++ src/Client.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
+import json
+
+from twisted.internet import reactor
+from autobahn.twisted.websocket import WebSocketClientFactory, WebSocketClientProtocol, \
+	connectWS
+
 class Client():
 	
-	def __init__(self, ClientProtocol, product_ids=[], channels=[]):
+	def __init__(self, ClientProtocol, webSocketFeedUrl):
 		self.ClientProtocol = ClientProtocol
-		self.product_ids = product_ids
-		self.channels = {}
-		for channel in channels:
-			self.channels[channel] = self.product_ids
-	
-	def addToIndividualChannel(self, channel, product_ids=[]):
-		self.channels[channel] = self.product_ids
-		for product_id in product_ids:
-			self.channels[channel].append(product_id)
+		self.webSocketFeedUrl = webSocketFeedUrl
+		self.factory = WebSocketClientFactory(webSocketFeedUrl)
+		self.factory.protocol = self.ClientProtocol
+
+	def startClient(self):
+		connectWS(self.factory)
+		reactor.run()
