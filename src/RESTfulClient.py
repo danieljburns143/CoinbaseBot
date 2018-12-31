e54c7d22469f131b1f3f6791289d5541ed0bed49
Daniel Burns <danieljburns143@gmail.com>
2018-12-31 21:17:50 +0000

    Fix: RESTfulClient methods return response object now

--- src/RESTfulClient.py
+++ src/RESTfulClient.py
@@ -18,15 +18,13 @@ class RESTfulClient():
 	def getProducts(self):
 		url = self.endpointURL + '/products'
 		r = requests.get(url)
-		return json.loads(r.text)
+		return r
 
 	def getProductOrderBook(self, productID, level='1'):
-		if int(level) < 0 or int(level) > 3:
-			raise ValueError('Invalid level')
 		url = self.endpointURL + '/products/{}/book'.format(productID)
 		parameters = {'level': level}
 		r = requests.get(url, params=parameters)
-		return json.loads(r.text)
+		return r
 	
 	'''
 	{'trade_id': x, 'price': 'x', 'size': 'x', 'bid': 'x', 'ask': 'x', 'volume':'x', 'time':'x'}
@@ -34,7 +32,7 @@ class RESTfulClient():
 	def getTicker(self, productID):
 		url = self.endpointURL + '/products/{}/ticker'.format(productID)
 		r = requests.get(url)
-		return json.loads(r.text)
+		return r
 
 	'''
 	[ {'time': 'x', 'trade_id': x, 'price': 'x', 'size': 'x', 'side': 'x'}, ... ]
@@ -42,20 +40,18 @@ class RESTfulClient():
 	def getLatestTrades(self, productID):
 		url = self.endpointURL + '/products/{}/trades'.format(productID)
 		r = requests.get(url)
-		return json.loads(r.text)
+		return r
 
 	'''
 	[ [time, low, high, open, close, volume], ... ]
 	'''
 	def getHistoricRates(self, productID, granularity, start=None, end=None):
-		if granularity not in {'60', '300', '900', '3600', '21600', '86400'}:
-			raise ValueError('Invalid granularity')
 		url = self.endpointURL + '/products/{}/candles'.format(productID)
 		parameters = {'granularity': granularity}
 		if start: parameters['start'] = start
 		if end: parameters['end'] = end
 		r = requests.get(url, params=parameters)
-		return json.loads(r.text)
+		return r
 	
 	'''
 	{ 'open': 'x', 'high': 'x', 'low': 'x', 'volume': 'x' }
@@ -63,4 +59,4 @@ class RESTfulClient():
 	def get24HourStats(self, productID):
 		url = self.endpointURL + '/product/{}/statds'.format(productID)
 		r = requests.get(url)
-		return json.loads(r.text)
+		return r
